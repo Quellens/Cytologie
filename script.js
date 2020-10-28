d848d974a46d33a011bb39753a9e8a03f7711c5b
Quellens <joelseckershady@gmail.com>
2020-10-28 16:44:07 +0100

    PROGRESS: Loading 2twoÂ3D Models and make it switch dynamically

--- script.js
+++ script.js
@@ -6,14 +6,20 @@
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-undef */
 
+let button = document.createElement("button");
 const clock = new THREE.Clock();
+
 
 function main() {
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(75, 600 / 400, 1, 5000);
+  const container = document.getElementById('container');
+  const renderer = new THREE.WebGLRenderer( {antialias: true});
   const light1 = new THREE.PointLight(0x404040, 3);
+  light1.name = "light";
   light1.position.set(0, 70, 70);
   const light2 = new THREE.PointLight(0x404040, 5);
+  light2.name = "light";
   light2.position.set(70,70,0)
   scene.background = new THREE.Color(0xD1D1D1);
   camera.rotation.y = 45/180*Math.PI;
@@ -26,16 +32,44 @@ function main() {
 
 
   let loader = new THREE.GLTFLoader();
-  loader.load("img/scene.gltf", (gltf) => {
-    const cell = gltf.scene;
-    cell.scale.set(10, 10, 10);
-    cell.receiveShadow = true;
-    cell.castShadow = true;
-  
-    scene.add(cell);
-  });
+    loader.load("img/plant/scene.gltf", (gltf) => {
+      const cell = gltf.scene;
+        cell.scale.set(10, 10, 10);
+        cell.position.y += 5;
+      scene.add(cell);
+    });
+  let loader2 = new THREE.GLTFLoader();
+    loader2.load("img/animal/scene.gltf", (gltf) => {
+      const cell = gltf.scene;
+      cell.scale.set(5,5,5);
+      cell.position.y =4;
+      cell.position.x = 2;
+      cell.position.z = -4;
+      cell.visible = false;
+      scene.add(cell);
+      button.innerText = "⏭";
+      container.appendChild(button);
+    });
 
-  const renderer = new THREE.WebGLRenderer( {antialias: true});
+
+  var bool = false;
+   button.addEventListener("click", () => {
+    const heading = document.getElementById("heading");
+    var models = scene.children.filter(child => child.name != "light");
+    if(bool){
+      models[0].visible = true;
+      heading.innerText = "Pflanzenzelle:";
+      models[1].visible = false;
+    }
+    if(!bool){
+      models[1].visible = true;
+      heading.innerText = "Tierzelle:";
+      models[0].visible = false;
+    }
+    bool = !bool;
+   });
+
+ 
 
   const controls = new THREE.OrbitControls(camera, renderer.domElement);
   controls.enableDamping = true;
@@ -44,8 +78,8 @@ function main() {
   controls.screenSpacePanning = false;
   controls.minDistance = 7;
   controls.maxDistance = 22;
-  controls.maxPolarAngle = Math.PI / 2.1;
-  const container = document.getElementById('container');
+  controls.maxPolarAngle = Math.PI / 2.7;
+ 
   renderer.setSize(600, 400);
   container.appendChild(renderer.domElement);
   renderer.render(scene, camera);
